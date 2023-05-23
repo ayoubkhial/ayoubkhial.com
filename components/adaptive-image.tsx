0c5ecda5634e75983a05478741cc97a893de77c0
Ayoub Khial <ayoubcodez@gmail.com>
2023-05-24 00:57:07 +0100

    Add a Dynamic Image component

---
+++ components/adaptive-image.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+
+type Props = {
+	src: string;
+	alt: string;
+	width: number;
+	height: number;
+};
+
+export default function AdaptiveImage({ src, alt, width, height }: Props) {
+	const { resolvedTheme } = useTheme();
+	const [extension, ...filename] = src.split('.').reverse();
+	const source = resolvedTheme === 'dark' ? `${filename}_dark.${extension}` : src;
+	return (
+		<Image
+			className="my-6 rounded-xl border border-gray-100 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-900"
+			src={source}
+			alt={alt}
+			width={width}
+			height={height}
+		/>
+	);
+}
