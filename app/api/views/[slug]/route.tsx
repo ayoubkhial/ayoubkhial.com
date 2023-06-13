b39bdcbbcfd2d50adcc7d413b30e5cfb796dd5e3
Ayoub Khial <ayoubcodez@gmail.com>
2023-06-13 23:10:44 +0100

    Move from prisma to kysely

--- app/api/views/[slug]/route.tsx
+++ app/api/views/[slug]/route.tsx
@@ -1,19 +1,23 @@
-import prisma from '@lib/prisma';
+import { queryBuilder } from '@lib/planetscale';
 import { NextRequest, NextResponse } from 'next/server';
 
 const GET = async (_: NextRequest, context: { params: { slug: string } }) => {
 	const { slug } = context.params;
-	const postInfo = await prisma.postInfo.findFirst({ where: { slug } });
+	const postInfo = await queryBuilder.selectFrom('PostInfo').select('views').where('slug', '=', slug).executeTakeFirst();
 	let views = 1;
-	if (!postInfo) await prisma.postInfo.create({ data: { slug, views: 1 } });
+	if (!postInfo) await queryBuilder.insertInto('PostInfo').values({ slug, views: 1 }).executeTakeFirst();
 	else views = postInfo.views;
 	return NextResponse.json({ views });
 };
 
 const PUT = async (_: NextRequest, context: { params: { slug: string } }) => {
 	const { slug } = context.params;
-	const { views } = await prisma.postInfo.update({ where: { slug }, data: { views: { increment: 1 } } });
-	return NextResponse.json({ views });
+	const data = await queryBuilder
+		.updateTable('PostInfo')
+		.set(eb => ({ views: eb.bxp('views', '+', 1) }))
+		.where('slug', '=', slug)
+		.executeTakeFirst();
+	return NextResponse.json({ updated: data.numUpdatedRows });
 };
 
 export { GET, PUT };
---
+++ lib/planetscale.ts
@@ -0,0 +1,18 @@
+import { Generated, Kysely } from 'kysely';
+import { PlanetScaleDialect } from 'kysely-planetscale';
+
+interface PostInfo {
+	id: Generated<number>;
+	slug: string;
+	views: number;
+}
+
+interface Database {
+	PostInfo: PostInfo;
+}
+
+export const queryBuilder = new Kysely<Database>({
+	dialect: new PlanetScaleDialect({
+		url: process.env.DATABASE_URL
+	})
+});
