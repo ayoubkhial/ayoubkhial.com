5b1b0795f647a605467f0ffbb0492ab979dd86f1
Ayoub Khial <ayoubcodez@gmail.com>
2023-06-26 23:34:59 +0100

    Change some colors for better UI

--- components/callout.tsx
+++ components/callout.tsx
@@ -9,12 +9,12 @@ export default function callout({ type, children }: Props) {
 	const color = type === 'INFO' ? 'blue' : type === 'WARNING' ? 'red' : 'purple';
 	const calloutColorVariants = {
 		blue: 'border-[#99cfff] bg-[#F0F8FF] dark:border-[#b3dbff] dark:bg-[#003666]',
-		red: 'border-[#ffb3c6] bg-[#FFF3F6] dark:border-[#f5bdc0] dark:bg-[#dd222c]',
+		red: 'border-[#ffb3c6] bg-[#FFF3F6] dark:border-[#fad1d1] dark:bg-[#B31312]',
 		purple: 'border-[#cc9dfb] bg-[#f2e6fe] dark:border-[#c8d2e9] dark:bg-[#1d2a49]'
 	};
 	const textColorVariants = {
 		blue: 'text-[#00294d] dark:text-[#e5f3ff]',
-		red: 'text-[#4d0013] dark:text-[#fef6f7]',
+		red: 'text-[#4d0013] dark:text-[#fef1f1]',
 		purple: 'text-[#26034a] dark:text-[#dae1f1]'
 	};
 	return (
