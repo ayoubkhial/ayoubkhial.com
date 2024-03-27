adc2eb94e14ba27632edba35adf4512336998a62
Ayoub KHIAL <ayoubcodez@gmail.com>
2024-03-27 01:05:16 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,19 @@ import Image from 'next/image';
 const HeroSection = () => {
   return (
     <>
-      <section className="mx-2 rounded-lg bg-solitude-50 lg:flex lg:items-center lg:gap-4 xl:mx-0 xl:w-[1280px] dark:bg-solitude-900">
+      <section className="mx-2 rounded-lg bg-solitude-50 dark:bg-solitude-900 lg:flex lg:items-center lg:gap-4 xl:mx-0 xl:w-[1280px]">
         <div className="p-4 lg:w-2/3 lg:p-8 lg:pr-0 xl:w-3/4">
           <div className="mb-6 md:mb-0">
-            <p className="mb-1 font-mono text-xs font-extrabold tracking-wide text-solitude-900 lg:mb-2 lg:text-sm dark:text-solitude-200">
+            <p className="mb-1 font-mono text-xs font-extrabold tracking-wide text-solitude-900 dark:text-solitude-200 lg:mb-2 lg:text-sm">
               Hello, my name is
             </p>
-            <h1 className="mb-2 text-2xl font-bold text-solitude-950 sm:text-3xl lg:mb-4 xl:text-4xl dark:text-solitude-100">
+            <h1 className="mb-2 text-2xl font-bold text-solitude-950 dark:text-solitude-100 sm:text-3xl lg:mb-4 xl:text-4xl">
               Ayoub Khial
             </h1>
-            <p className="mb-3 text-sm font-medium tracking-tight text-solitude-900 lg:mb-6 lg:text-base dark:text-solitude-200">
+            <p className="mb-3 text-sm font-medium tracking-tight text-solitude-900 dark:text-solitude-200 lg:mb-6 lg:text-base">
               Welcome to my digital space, where I spill the beans on everything related to web development.
             </p>
-            <p className="text-sm leading-relaxed text-solitude-900 lg:text-base lg:leading-7 dark:text-solitude-200">
+            <p className="text-sm leading-relaxed text-solitude-900 dark:text-solitude-200 lg:text-base lg:leading-7">
               I’m a web developer with a knack for crafting beautiful, minimalist web experiences. My journey right now is all about
               mastering the MEAN stack where I team up with creative minds to forge business solutions that are not just efficient, but also
               strikingly impactful.
@@ -37,7 +37,7 @@ const HeroSection = () => {
               aria-label="Github"
             >
               <GithubIcon />
-              <span className="hidden text-gray-700 underline decoration-gray-300 decoration-2 underline-offset-2 transition duration-300 hover:decoration-gray-400 sm:inline-block dark:text-gray-400 dark:decoration-gray-700 dark:hover:decoration-gray-600">
+              <span className="hidden text-gray-700 underline decoration-gray-300 decoration-2 underline-offset-2 transition duration-300 hover:decoration-gray-400 dark:text-gray-400 dark:decoration-gray-700 dark:hover:decoration-gray-600 sm:inline-block">
                 Github
               </span>
             </Link>
@@ -49,7 +49,7 @@ const HeroSection = () => {
               aria-label="Twitter"
             >
               <TwitterIcon />
-              <span className="hidden text-slate-800 underline decoration-slate-300 decoration-2 underline-offset-2 transition duration-300 hover:decoration-slate-400 sm:inline-block dark:text-slate-400 dark:decoration-slate-700 dark:hover:decoration-slate-600">
+              <span className="hidden text-slate-800 underline decoration-slate-300 decoration-2 underline-offset-2 transition duration-300 hover:decoration-slate-400 dark:text-slate-400 dark:decoration-slate-700 dark:hover:decoration-slate-600 sm:inline-block">
                 Twitter
               </span>
             </Link>
@@ -61,7 +61,7 @@ const HeroSection = () => {
               aria-label="LinkedIn"
             >
               <LinkedInIcon />
-              <span className="hidden text-solitude-700 underline decoration-solitude-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-solitude-400 sm:inline-block dark:text-solitude-400 dark:decoration-solitude-700 dark:hover:decoration-solitude-600">
+              <span className="hidden text-solitude-700 underline decoration-solitude-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-solitude-400 dark:text-solitude-400 dark:decoration-solitude-700 dark:hover:decoration-solitude-600 sm:inline-block">
                 LinkedIn
               </span>
             </Link>
@@ -73,13 +73,13 @@ const HeroSection = () => {
               aria-label="Email"
             >
               <MessageIcon />
-              <span className="hidden text-chablis-600 underline decoration-chablis-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-chablis-400 sm:inline-block dark:text-chablis-400 dark:decoration-chablis-700 dark:hover:decoration-chablis-600">
+              <span className="hidden text-chablis-600 underline decoration-chablis-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-chablis-400 dark:text-chablis-400 dark:decoration-chablis-700 dark:hover:decoration-chablis-600 sm:inline-block">
                 Email
               </span>
             </Link>
             <Link href="/resume.pdf" download={true} className="flex items-center gap-2" aria-label="Resume">
               <LinkIcon />
-              <span className="hidden text-madang-600 underline decoration-madang-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-madang-400 sm:inline-block dark:text-madang-400 dark:decoration-madang-700 dark:hover:decoration-madang-600">
+              <span className="hidden text-madang-600 underline decoration-madang-200 decoration-2 underline-offset-2 transition duration-300 hover:decoration-madang-400 dark:text-madang-400 dark:decoration-madang-700 dark:hover:decoration-madang-600 sm:inline-block">
                 Resume
               </span>
             </Link>
@@ -104,7 +104,7 @@ const BlogSection = () => {
     <>
       <section className="mx-2 xl:mx-0 xl:w-[1280px]">
         <div className="mb-4 flex gap-2 lg:mb-6 lg:gap-4">
-          <h2 className="text-xl font-semibold tracking-tight underline decoration-slate-200 decoration-4 underline-offset-4 lg:text-2xl dark:decoration-slate-700">
+          <h2 className="text-xl font-semibold tracking-tight underline decoration-slate-200 decoration-4 underline-offset-4 dark:decoration-slate-700 lg:text-2xl">
             The latest from my blog
           </h2>
           <Link
@@ -115,7 +115,7 @@ const BlogSection = () => {
           </Link>
         </div>
 
-        <p className="leading-txt mb-4 text-sm leading-relaxed text-slate-700 lg:mb-6 lg:text-base lg:leading-7 dark:text-slate-400">
+        <p className="leading-txt mb-4 text-sm leading-relaxed text-slate-700 dark:text-slate-400 lg:mb-6 lg:text-base lg:leading-7">
           I’ve been sharing insights through my blog and newsletter for the past year. My approach is to simplify complex topics. You’ll
           discover posts about the latest technologies catching my interest alongside my professional growth and learning journey. As I
           evolve in my career, I enjoy passing on the knowledge I’ve gained.
@@ -133,12 +133,12 @@ const BlogSection = () => {
 const NewsletterSection = () => {
   return (
     <>
-      <section className="mx-2 rounded-lg bg-solitude-50 lg:flex lg:items-center lg:gap-4 xl:mx-0 xl:w-[1280px] dark:bg-solitude-900">
+      <section className="mx-2 rounded-lg bg-solitude-50 dark:bg-solitude-900 lg:flex lg:items-center lg:gap-4 xl:mx-0 xl:w-[1280px]">
         <div className="p-4 lg:w-3/4 lg:p-8 lg:pr-0">
-          <h2 className="mb-4 text-xl font-semibold tracking-tight text-solitude-950 underline decoration-slate-200 decoration-4 underline-offset-4 lg:mb-6 lg:text-2xl dark:text-solitude-200 dark:decoration-slate-700">
+          <h2 className="mb-4 text-xl font-semibold tracking-tight text-solitude-950 underline decoration-slate-200 decoration-4 underline-offset-4 dark:text-solitude-200 dark:decoration-slate-700 lg:mb-6 lg:text-2xl">
             Subscribe to my newsletter
           </h2>
-          <p className="mb-3 text-sm leading-relaxed text-solitude-900 lg:mb-6 lg:text-base lg:leading-7 dark:text-solitude-300">
+          <p className="mb-3 text-sm leading-relaxed text-solitude-900 dark:text-solitude-300 lg:mb-6 lg:text-base lg:leading-7">
             Get notified whenever I share new articles by subscribing to my newsletter. Also, I share exciting tools and resources I found
             while surfing the web.
           </p>
@@ -161,7 +161,7 @@ const NewsletterSection = () => {
             >
               <span className="text-sm font-medium text-solitude-900 dark:text-solitude-200">Explore Previous Issues</span>
             </Link>
-            <span className="ml-1 text-xs text-solitude-950 underline lg:text-sm dark:text-solitude-200">
+            <span className="ml-1 text-xs text-solitude-950 underline dark:text-solitude-200 lg:text-sm">
               No spam. I only send you relevant content. Unsubscribe at any time.
             </span>
           </div>
--- components/views-counter.tsx
+++ components/views-counter.tsx
@@ -1,24 +1,32 @@
-import { queryBuilder } from '@lib/planetscale';
 import { unstable_noStore as noStore } from 'next/cache';
+import { createClient } from '../supabase/server';
 
-export const incrementViews = (slug: string) => {
-  queryBuilder
-    .updateTable('PostInfo')
-    .set((eb) => ({ views: eb('views', '+', 1) }))
-    .where('slug', '=', slug)
-    .execute();
+export const incrementViews = async (slug: string) => {
+  const supabase = createClient();
+  const { data } = await supabase.from('PostInfo').select('views').eq('slug', slug).maybeSingle();
+  if (!data) {
+    await supabase.from('PostInfo').insert([{ slug, views: 1 }]);
+  } else {
+    await supabase
+      .from('PostInfo')
+      .update({ views: data.views + 1 })
+      .eq('slug', slug);
+  }
 };
 
 const ViewsCounter = async ({ slug }) => {
   noStore();
-  let post;
-  try {
-    post = await queryBuilder.selectFrom('PostInfo').selectAll().where('slug', '=', slug).executeTakeFirst();
-    if (!post) {
-      await queryBuilder.insertInto('PostInfo').values({ slug, views: 1 }).execute();
-    }
-  } catch {
+  let post: { views: number };
+  const supabase = createClient();
+
+  const { data, error } = await supabase.from('PostInfo').select('views').eq('slug', slug).maybeSingle();
+  if (!data) {
+    await supabase.from('PostInfo').insert([{ slug, views: 1 }]);
+  } else if (error) {
     post = { views: 1 };
+  }
+  {
+    post = data;
   }
 
   return <span>{post?.views} views</span>;
